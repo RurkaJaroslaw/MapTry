15e10fc6ecceae7fd35a92f7565c79d2476d3b38
RurkaJaroslaw <jarek26397@gmail.com>
2020-11-11 20:46:22 +0100

    Map Layers added

--- app/src/main/java/com/example/mymap/MainActivity.java
+++ app/src/main/java/com/example/mymap/MainActivity.java
@@ -30,7 +30,7 @@ public class MainActivity extends AppCompatActivity {
         button2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent myIntent = new Intent(MainActivity.this, MapsActivity.class);
+                Intent myIntent = new Intent(MainActivity.this, LayersActivity.class);
                 MainActivity.this.startActivity(myIntent);
             }
         });
